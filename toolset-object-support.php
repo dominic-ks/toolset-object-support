992048a5c010c315c065e1c0df083f3bef88e4e3
Dominic Vermeulen-Smith <contact@bedevious.co.uk>
2019-06-09 13:19:23 +0100

    updated version tag

--- toolset-object-support.php
+++ toolset-object-support.php
@@ -5,7 +5,7 @@
 * Plugin Name: Toolset Object Support
 * Plugin URI: https://www.bedevious.co.uk/
 * Description: NB - current version developed for The Agility Club
-* Version: 0.2.1
+* Version: 0.2.2
 * Author: Be Devious Web Development
 * Author URI: Plugin URI: https://www.bedevious.co.uk/
 * License: GNU GENERAL PUBLIC LICENSE V3
